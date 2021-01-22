cf25c4a5799df7a33dd8d00c9416822e2e1809e9
Valentina-Olekhnovich <volekh@gmail.com>
2021-01-22 15:48:42 +0100

    split code between files

--- src/actions/goals.js
+++ src/actions/goals.js
@@ -1,6 +1,41 @@
-export const ADD_TODO = "ADD_TODO";
-export const REMOVE_TODO = "REMOVE_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-export const ADD_GOAL = "ADD_GOAL";
-export const REMOVE_GOAL = "REMOVE_GOAL";
-export const RECEIVE_DATA = "RECEIVE_DATA";
+import API from 'goals-todos-api'
+
+export const ADD_GOAL = 'ADD_GOAL'
+export const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addGoal (goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  }
+}
+
+function removeGoal (id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  }
+}
+
+export function handleAddGoal (name, cb) {
+  return (dispatch) => {
+    return API.saveGoal(name)
+      .then((goal) => {
+        dispatch(addGoal(goal))
+        cb()
+      })
+      .catch(() => alert('There was an error. Try again.'))
+  }
+}
+
+export function handleDeleteGoal (goal) {
+  return (dispatch) => {
+    dispatch(removeGoal(goal.id))
+
+    return API.deleteGoal(goal.id)
+      .catch(() => {
+        dispatch(addGoal(goal))
+        alert('An error occurred. Try again.')
+      })
+  }
+} 
